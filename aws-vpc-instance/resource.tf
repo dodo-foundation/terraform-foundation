6cef5082b56ac221e38ceefc8104ff78d2ae3029
fourtimes <jino@gmail.com>
2022-02-17 22:14:46 -0500

    vpc creation added

---
+++ aws-vpc-instance/resource.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "av" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.PROJECT}-${var.ENVIRONMENT}-vpc"
+  }
+}
+
+resource "aws_subnet" "as" {
+  vpc_id     = aws_vpc.av.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "${var.PROJECT}-${var.ENVIRONMENT}-subnet"
+  }
+}
---
+++ aws-vpc-instance/variable.tf
@@ -0,0 +1,3 @@
+variable "ENVIRONMENT" {}
+variable "PROJECT" {}
+
