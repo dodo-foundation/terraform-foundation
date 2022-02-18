f20d40acede04aa1bcb34b7f5872714157f00534
fourtimes <jino@gmail.com>
2022-02-17 22:44:42 -0500

    variable splits

--- aws-vpc-instance/variable.tf
+++ aws-vpc-instance/variable.tf
@@ -1,17 +1,5 @@
 variable "ENVIRONMENT" {}
 variable "PROJECT" {}
-
-variable "AMI" {
-  default = "ami-04505e74c0741db8d"
-}
-
-variable "VPC_CIDR_BLOCK" {
-  default = "10.0.0.0/16"
-}
-
-variable "SUBNET_CIDR_BLOCK" {
-  default = "10.0.1.0/24"
-}
-
-
-
+variable "AMI" {}
+variable "VPC_CIDR_BLOCK" {}
+variable "SUBNET_CIDR_BLOCK" {}
