6df0fb62215fc0cdc5100ee37443d19f4dbccdf2
fourtimes <jino@gmail.com>
2022-03-02 09:03:10 +0530

    userdata and keyfile added

--- aws-vpc-instance/script/main.sh
+++ aws-vpc-instance/script/main.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 sudo apt update
-sudo apt install apache2 jenkins -y
+sudo apt install apache2 git -y
 sudo systemctl start apache2
-sudo systemctl start jenkins
 sudo systemctl enable apache2
