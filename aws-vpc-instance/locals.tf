d995a3c3fdddb6fd10caa7aa2e3f0103edc2ce29
fourtimes <jino@gmail.com>
2022-03-03 09:16:56 +0530

    condition added

--- aws-vpc-instance/locals.tf
+++ aws-vpc-instance/locals.tf
@@ -2,6 +2,6 @@ locals {
   INSTANCE_ENV         = var.ENVIRONMENT == "prod" ? "production" : "non-production"
   INSTANCE_TYPE        = var.ENVIRONMENT == "prod" ? "t2.micro" : "t2.micro"
   ADD_INSTANCE_ENV     = local.INSTANCE_ENV == "production" ? "krishna" : "vignesh"
-  ADD_ADD_INSTANCE_ENV = local.ADD_INSTANCE_ENV == "krishna" ? "junior" : "senior"
+  ADD_ADD_INSTANCE_ENV = lower(local.ADD_INSTANCE_ENV == "krishna" ? "junior" : "senior")
   SELECT_NUMBER        = max(12, 54, 3)
 }
