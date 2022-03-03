70645d20f05f359d5f1f636296082b17c4c23d10
fourtimes <jino@gmail.com>
2022-03-03 09:03:10 +0530

    conditional statement added

--- aws-vpc-instance/locals.tf
+++ aws-vpc-instance/locals.tf
@@ -1,5 +1,7 @@
 locals {
-  INSTANCE_ENV  = var.ENVIRONMENT == "prod" ? "production" : "non-production"
-  INSTANCE_TYPE = var.ENVIRONMENT == "prod" ? "t2.medium" : "t2.micro"
-  ADD_INSTANCE_ENV = local.INSTANCE_ENV == "production" ? "krishna" : "vignesh"
+  INSTANCE_ENV         = var.ENVIRONMENT == "prod" ? "production" : "non-production"
+  INSTANCE_TYPE        = var.ENVIRONMENT == "prod" ? "t2.medium" : "t2.micro"
+  ADD_INSTANCE_ENV     = local.INSTANCE_ENV == "production" ? "krishna" : "vignesh"
+  ADD_ADD_INSTANCE_ENV = local.ADD_INSTANCE_ENV == "krishna" ? "junior" : "senior"
+
 }
