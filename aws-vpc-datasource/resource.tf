b3b1061c63e2752322eec871bb90113c37b89874
fourtimes <jino@gmail.com>
2022-03-13 09:26:25 +0530

    datasource with variable call

--- aws-vpc-datasource/resource.tf
+++ aws-vpc-datasource/resource.tf
@@ -7,3 +7,22 @@ resource "aws_subnet" "main" {
   }
 }
 
+resource "aws_iam_role" "air_lambda" {
+  name = "${var.PROJECT}-${var.ENVIRONMENT}-lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
