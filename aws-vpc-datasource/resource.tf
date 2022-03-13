26cde225d522c262fb03edaf137bce730563117f
fourtimes <jino@gmail.com>
2022-03-13 09:27:16 +0530

    datasource with variable call

--- aws-vpc-datasource/resource.tf
+++ aws-vpc-datasource/resource.tf
@@ -1,9 +1,8 @@
 resource "aws_subnet" "main" {
   vpc_id     = data.aws_vpc.av.id
   cidr_block = "172.31.112.0/20"
-
   tags = {
-    Name = "add-subnet"
+    Name = "${var.PROJECT}-${var.ENVIRONMENT}-subnet"
   }
 }
 
